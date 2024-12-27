b16987ac2f6b7f8fa179f9a3e123087b10121764
satish-kalepu <ksatish21@gmail.com>
2024-12-27 23:19:19 +0530

    updates 20241227_231919

---
+++ apimaker/page_apps_pages_page_html_2.php
@@ -0,0 +1,9 @@
+<?php
+
+$apps_folder = "apps_";
+
+$_use_encrypted_scripts = true;
+
+require("page_apps_pages_page_html_css_2.php"); 
+require("page_apps_pages_page_html_template_2.php"); 
+require("page_apps_pages_page_html_script_2.php");
